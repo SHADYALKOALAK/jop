6d3bca6d3904fada2ba7ea4212077e02514d177b
living_space <sa187@smail.ucas.edu.ps>
2023-07-11 21:05:44 +0300

    up

--- app/src/main/java/com/example/alesraaprojectxml/ReplyAdmin.java
+++ app/src/main/java/com/example/alesraaprojectxml/ReplyAdmin.java
@@ -13,27 +13,31 @@ import com.example.alesraaprojectxml.databinding.CustomdailogBinding;
 
 public class ReplyAdmin extends AppCompatActivity {
     private CustomdailogBinding customDialog;
-    Context context=ReplyAdmin.this;
+    Context context = ReplyAdmin.this;
     private AlertDialog dialog;
+    private DBase dBase;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityReplyAdminBinding binding;
-        binding=ActivityReplyAdminBinding.inflate(getLayoutInflater());
+        binding = ActivityReplyAdminBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        dBase = new DBase(context);
         binding.btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (binding.editNamePerson.getText().toString().isEmpty()) {
+                String massage = binding.editNamePerson.getText().toString().trim();
+                if (massage.isEmpty()) {
                     binding.editNamePerson.setError("من فضلك أدخل نص الرسالة أولاً");
-                }else{
+                } else {
                     AlertDialog.Builder builder = new AlertDialog.Builder(context);
                     customDialog = CustomdailogBinding.inflate(getLayoutInflater());
                     builder.setView(customDialog.getRoot());
                     customDialog.btnYes.setOnClickListener(v1 -> {
-                            startActivity(new Intent(context,SureSend_2.class));
+                        startActivity(new Intent(context, SureSend_2.class));
+                        dBase.insertNotifications("تم الرد من قبل المحاضر" +massage);
                         overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
                     });
                     customDialog.btnNo.setOnClickListener(v12 -> {
@@ -53,9 +57,6 @@ public class ReplyAdmin extends AppCompatActivity {
 
             }
         });
-
-
-
 
 
     }
